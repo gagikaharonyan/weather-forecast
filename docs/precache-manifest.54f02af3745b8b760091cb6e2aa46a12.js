315054f79b9e520669c80b15dab092062789ea2e
GaGo Aharonyan <gago9639@gmail.com>
2020-09-14 22:10:12 +0400

    added gh pages version

---
+++ docs/precache-manifest.54f02af3745b8b760091cb6e2aa46a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec039d92d37275b7f13c205b1417736",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "f7afdf0093879c2d89d0",
+    "url": "/weather-forecast/static/css/main.bdbb88f3.chunk.css"
+  },
+  {
+    "revision": "af694ba1dbf332bb6496",
+    "url": "/weather-forecast/static/js/2.8cf968c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-forecast/static/js/2.8cf968c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7afdf0093879c2d89d0",
+    "url": "/weather-forecast/static/js/main.ad187135.chunk.js"
+  },
+  {
+    "revision": "755d4eaeb80cc5e1e57e",
+    "url": "/weather-forecast/static/js/runtime-main.e98e7fa7.js"
+  }
+]);
